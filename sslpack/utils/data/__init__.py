32b2c1e1c079cb959b264ac2af4f18ce997b5930
Nazeef Hamid <nazeef.h.289@gmail.com>
2025-09-05 15:05:16 +1000

    fix: fix incorrect imports for new dataloaders

--- sslpack/utils/data/__init__.py
+++ sslpack/utils/data/__init__.py
@@ -1,3 +1,9 @@
 from .data_utils import split_lb_ulb_balanced
 from .dataset import BasicDataset, TransformDataset
-from .dataloader import SSLDataLoader, MinimumLoader, CyclicLoader
+from .dataloader import (
+    SSLDataLoader,
+    MinimumLoader,
+    LabelledEpochLoader,
+    UnlabelledEpochLoader,
+    CyclicLoader
+)
