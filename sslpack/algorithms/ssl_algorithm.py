53f2d78c823dd3539af9b1a172ae45ee2ad232f8
Nazeef Hamid <n.hamid@uq.net.au>
2026-05-04 18:09:18 +1000

    refactor: ssl_algorithm now extends nn.Module for easier device management when using sstateful algorithms eg. FreeMatch, FlexMatch

--- sslpack/algorithms/ssl_algorithm.py
+++ sslpack/algorithms/ssl_algorithm.py
@@ -1,15 +1,13 @@
-class Algorithm:
+from torch import nn
+
+
+class Algorithm(nn.Module):
 
     def __init__(self):
-        pass
+        super().__init__()
 
     def forward(self, model, lbl_batch: dict, ulbl_batch: dict, log_func=None):
         """
         forward specific to each algorithm
         """
-        # implement train step for each algorithm
-        # compute loss
-        # update model
-        # log
-        # return loss
         raise NotImplementedError
